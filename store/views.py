4eae8f70deb18100b9215bbbf9d625bfd5a1de58
Biswasmit Lenka <biswasmitlenka@gmail.com>
2024-05-15 01:47:26 +0530

    Few Updates to Initial Release

--- store/views.py
+++ store/views.py
@@ -5,4 +5,9 @@ from .models import Product
 def home(request):
 
     products=Product.objects.all()
-    return render(request, 'home.html', {'products':products})
+    return render(request, 'Home.html', {'products':products})
+
+def your_view(request):
+    # Assuming you have a variable named 'motorcycle_background_image' that contains the URL of the motorcycle background image
+    motorcycle_background_image = '/Users/biswasmitlenka/website/ecom/static/assets/motorcycle-background.jpg'
+    return render(request, 'Home.html', {'motorcycle_background_image': motorcycle_background_image})
